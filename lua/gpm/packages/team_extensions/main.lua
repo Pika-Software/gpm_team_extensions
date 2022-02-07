37912a87b788a2f4bd7c4b56227ecf8925b1efea
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-08 01:26:22 +0400

    Initial commit

---
+++ lua/gpm/packages/team_extensions/main.lua
@@ -0,0 +1,5 @@
+local assert = assert
+local type = type
+
+local devLog = console.devLog
+local file_Exists = file.Exists
---
+++ lua/gpm/packages/team_extensions/package.lua
@@ -0,0 +1,12 @@
+return {
+    name = "Team Extensions",
+    description = "Expansion of the library of teams of players and objects.",
+    version = "1.0.0",
+    dependencies = {
+        ["Console Utils"] = ">=1.0.0",
+        ["Base Extensions"] = ">=1.0.0",
+        ["Environment Library"] = ">=1.0.0"
+    },
+    author = "PrikolMen:-b",
+    license = "MIT"
+}
