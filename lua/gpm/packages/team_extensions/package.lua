d06a8f9f06426e4b4bd6e91a37688c758ed0db53
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-08 02:04:13 +0400

    Removed unused dependencies

--- lua/gpm/packages/team_extensions/package.lua
+++ lua/gpm/packages/team_extensions/package.lua
@@ -3,8 +3,6 @@ return {
     description = "Expansion of the library of teams of players and objects.",
     version = "1.0.0",
     dependencies = {
-        ["Console Utils"] = ">=1.0.0",
-        ["Base Extensions"] = ">=1.0.0",
         ["Environment Library"] = ">=1.0.0"
     },
     author = "PrikolMen:-b",
