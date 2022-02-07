d8da5d9c3ace0554b341b72139824a54efe1b2c4
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-08 02:04:34 +0400

    Team System Extensions

--- lua/gpm/packages/team_extensions/main.lua
+++ lua/gpm/packages/team_extensions/main.lua
@@ -1,5 +1,119 @@
-local assert = assert
-local type = type
+local hook_Run = hook.Run
 
-local devLog = console.devLog
-local file_Exists = file.Exists
+--[[-------------------------------------------------------------------------
+	Team System Extensions
+---------------------------------------------------------------------------]]
+
+local TEAM_SPECTATOR = TEAM_SPECTATOR
+local TEAM_UNASSIGNED = TEAM_UNASSIGNED
+
+do
+
+    local team_SetUp = environment.saveFunc( "team.SetUp", team.SetUp )
+    function team.SetUp( ... )
+        if hook_Run( "PreTeamCreating", ... ) then
+            return
+        end
+
+        team_SetUp( ... )
+
+        hook_Run( "OnTeamCreated", ... )
+    end
+
+end
+
+--[[-------------------------------------------------------------------------
+	Logs ( works only with installed "Console Utils" )
+    https://github.com/Pika-Software/gpm_console_utils
+---------------------------------------------------------------------------]]
+
+do
+
+    hook.Add("OnTeamCreated", "game.Console", function( index, name, color )
+        console.devLog( "Team #" .. index .." Created - ", color, name ):setTag( "Team Extensions" ):setSeparator()
+    end)
+
+end
+
+--[[-------------------------------------------------------------------------
+	Player Extensions
+---------------------------------------------------------------------------]]
+
+local PLAYER = FindMetaTable("Player")
+
+function PLAYER:IsConnecting()
+	return self:Team() == TEAM_CONNECTING
+end
+
+if SERVER then
+
+    local RealTime = RealTime
+    local team_Joinable = team.Joinable
+
+    function PLAYER:JoinInTeam( teamid )
+        if not GAMEMODE.TeamBased then
+            GAMEMODE.TeamBased = true
+        end
+
+        if ( self:Team() == teamid ) then
+            self:ChatPrint( "You're already on that team" )
+            return false
+        end
+
+        if not team_Joinable( teamid ) then
+            self:ChatPrint( "You can't join that team" )
+            return false
+        end
+
+        local oldTeamid = self:Team()
+        if (hook_Run( "PlayerChangedTeam", self, oldTeamid, teamid ) == true) then
+            return false
+        end
+
+        self:SetTeam( teamid )
+        self["LastTeamSwitch"] = RealTime()
+
+        return true
+    end
+
+end
+
+--[[-------------------------------------------------------------------------
+	Entity Extensions
+---------------------------------------------------------------------------]]
+
+local ENTITY = FindMetaTable("Entity")
+
+if SERVER then
+
+    local team_Valid = team.Valid
+	function ENTITY:SetTeam( teamID )
+		self:SetNWInt( "__team", team_Valid( teamID ) and teamID or TEAM_UNASSIGNED )
+	end
+
+end
+
+function ENTITY:Team()
+	return self:GetNWInt( "__team", TEAM_UNASSIGNED )
+end
+
+--[[-------------------------------------------------------------------------
+	Global Extensions
+---------------------------------------------------------------------------]]
+
+function ENTITY:IsSpectator()
+	return self:Team() == TEAM_CONNECTING
+end
+
+function ENTITY:IsUnassigned()
+	return self:Team() == TEAM_UNASSIGNED
+end
+
+do
+
+    local team_GetColor = team.GetColor
+    function ENTITY:TeamColor()
+        return team_GetColor( self:Team() )
+    end
+
+end
